f3fcebcd8da84ecaa16959f444059d61e2038a6a
Teytey <steyssa.trancois@gmail.com>
2025-04-29 13:35:59 +0200

    script_connexion.js

--- script_connexion.js
+++ script_connexion.js
@@ -1,4 +1,4 @@
-// Selecting all required elements
+//SCRIPT POUR CONNEXION ACTIVE OU NON :
 const wrapper = document.querySelector(".wrapper"),
     toast = wrapper.querySelector(".toast"),
     title = toast.querySelector("span"),
@@ -7,39 +7,44 @@ const wrapper = document.querySelector(".wrapper"),
     closeIcon = toast.querySelector(".close-icon");
 window.onload = ()=>{
     function ajax(){
-        let xhr = new XMLHttpRequest(); //creating new XML object
-        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true); //sending get request on this URL
-        xhr.onload = ()=>{ //once ajax loaded
-            //if ajax status is equal to 200 or less than 300 that mean user is getting data from that provided url
-            //or his/her response status is 200 that means he/she is online
+        //crée un nouveau XML object (échange de donnée)
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+        xhr.onload = ()=>{
             if(xhr.status == 200 && xhr.status < 300){
                 toast.classList.remove("offline");
                 title.innerText = "You're online now";
                 subTitle.innerText = "Internet is connected !";
                 wifiIcon.innerHTML = '<i class="uil uil-wifi"></i>';
-                closeIcon.onclick = ()=>{ //hide toast notification on close icon click
+                //ne rends plus la notif visible quand on click sur l'icone de fermeture
+                closeIcon.onclick = ()=>{
                     wrapper.classList.add("hide");
                 }
-                setTimeout(()=>{ //hide the toast notification automatically after 5 seconds
+                //fermer la notif au bout de 5 secondes
+                setTimeout(()=>{
                     wrapper.classList.add("hide");
                 }, 5000);
             } else{
-                offline(); //calling offline function if ajax status is not equal to 200 or not less that 300
+                //appelé la fonction offline
+                offline();
             }
         }
         xhr.onerror = ()=>{
-            offline(); ////calling offline function if the passed url is not correct or returning 404 or other error
+            //pour toute erreur detecté, appellé la fonction offline
+            offline();
         }
-        xhr.send(); //sending get request to the passed url
+        //Envoie de la requête GET à l'URL passée
+        xhr.send();
     }
-    function offline(){ //function for offline
+    function offline(){
         wrapper.classList.remove("hide");
         toast.classList.add("offline");
         title.innerText = "You're offline now";
         subTitle.innerText = "Internet is disconnected !";
         wifiIcon.innerHTML = '<i class="uil uil-wifi-slash"></i>';
     }
-    setInterval(()=>{ //this setInterval function call ajax frequently after 100ms
+    //Appelle la fonction Ajax après 100 millisecondes
+    setInterval(()=>{ //
         ajax();
     }, 100);
 }
